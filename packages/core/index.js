59bfcc79d18c0e813408c6c7462e52ca203d4afd
YassineOuhadi <yassinouhadi@gmail.com>
2026-07-04 14:47:03 +0100

    Add JSON Schema for validation report output

--- packages/core/index.js
+++ packages/core/index.js
@@ -3,5 +3,6 @@
 module.exports = {
   ...require("./lib/typescript-check"),
   ...require("./lib/eslint-check"),
-  ...require("./lib/report")
+  ...require("./lib/report"),
+  reportSchema: require("./schema/validation-report.schema.json")
 };
